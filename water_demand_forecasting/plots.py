9b1cce7fd859f5391ede341cdc2e5ee4c76b9458
Diego Corredor <dcorredor20@hotmail.com>
2021-08-11 01:00:56 +0200

    Fix timestmps

--- water_demand_forecasting/plots.py
+++ water_demand_forecasting/plots.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def loss_plot(model, model_str: str, color: str):
     """Plot the loss plots."""
     f, ax = plt.subplots(1, 2, figsize=(18, 6))
-    ax[0].plot(model_def2.history["loss"], label=f"{model_str} loss", color=color)
+    ax[0].plot(model.history["loss"], label=f"{model_str} loss", color=color)
     ax[0].plot(
         model.history["val_loss"],
         label=f"{model_str} val loss",
@@ -13,9 +13,9 @@ def loss_plot(model, model_str: str, color: str):
         ls="--",
     )
     ax[0].legend()
-    ax[1].plot(model_def2.history["mse"], label=f"{model_str} MSE", color=color)
+    ax[1].plot(model.history["mse"], label=f"{model_str} MSE", color=color)
     ax[1].plot(
-        model_def2.history["val_mse"],
+        model.history["val_mse"],
         label=f"{model_str} val MSE",
         color=color,
         ls="--",
